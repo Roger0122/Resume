6e8740e571459abec3d8935177e0f2c59659d42b
黃俊龍 <a86527913@gmail.com~>
2025-05-16 16:28:16 +0800

    新增: 環境建置

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="bg-white p-6 rounded-2xl shadow-md text-center space-y-2">
+  <h2 class="text-xl font-bold text-gray-800">Vue 3</h2>
+  <p class="text-sm text-gray-500">熟悉 Composition API / Pinia</p>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
